662f5dd637e2bf27d9f234e661d47f942fcf3d04
FelipeODEUS <92820937+FelipeODEUS@users.noreply.github.com>
2022-01-27 18:03:35 -0300

    PROJETO FINAL

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
-var parede1, parede2, parede3, parede4, parede5, parede6, parede7, parede8, parede9, parede10, parede11, parede12,parede13,parede14,parede15,parede16;
-var jogador, Pc;
+var parede1, parede2, parede3, parede4, parede5, parede6, parede7, parede8, parede9, parede10, parede11, parede12,parede13,parede14,parede15,parede16,parede17,parede18,parede19;
+var jogador, Pc1, Pc2,Pc3;
 var fundo1,fundo2,fundo3;
 var luffy,Kaido;
-var Grupoparede1;
+var Grupoparede1, Grupoparede2, Grupoparede3;
 var estado = "Fase1";
-var Titan_Fundador,Titan_Blindado;
+var Titan_Fundador,Titan_Blindado,Asta,Rei_demonio;
+var trofeu;
+var ponto1,ponto2;
+var placar = 0;
+var borda;
 
 function preload(){
 fundo1 = loadImage("./Imagens/Tela de fundo 1.png");
@@ -13,31 +17,51 @@ luffy = loadAnimation("./Imagens/Luffy.png");
 Kaido = loadAnimation("./Imagens/Kaido.png");
 Titan_Fundador = loadAnimation("./Imagens/Titã Eren.png");
 Titan_Blindado = loadAnimation("./Imagens/Titã blindado.png");
+Asta = loadAnimation("./Imagens/Asta.png");
+Rei_demonio = loadAnimation("./Imagens/Rei demônio.png");
+trofeu = loadImage("./Imagens/troféu.png");
+fundo3 = loadImage("./Imagens/aa.jpg");
 }
-
 
 
 function setup(){
 createCanvas(400,400);
 
+borda = createEdgeSprites();
+
 Fase1();
-
-
 
 jogador = createSprite(50,140,15,15);
 jogador.addAnimation("Luffy", luffy);
+jogador.addAnimation("Eren",Titan_Fundador);
+jogador.addAnimation("asta",Asta);
 jogador.scale = 0.07;
 
-Pc = createSprite(120,50,15,15);
-Pc.addAnimation("Kaido", Kaido);
-Pc.scale = 0.1;
+Pc1 = createSprite(120,50,15,15);
+Pc1.addAnimation("Kaido", Kaido);
+Pc1.scale = 0.1;
 
-jogador.addAnimation("Eren",Titan_Fundador);
-Pc.addAnimation("Blindado",Titan_Blindado);
+Pc2 = createSprite(350,300,15,15);
+Pc2.addAnimation("Blindado",Titan_Blindado);
+Pc2.scale = 0.08;
+Pc2.velocityX = -2;
+Pc2.velocityY = 3;
+Pc2.visible = false;
 
+Pc3 = createSprite(350,300,15,15);
+Pc3.addAnimation("rei_demonio",Rei_demonio );
+Pc3.scale = 0.2;
+Pc3.velocityX = -2;
+Pc3.velocityY = 3;
+Pc3.visible = false;
+
+ponto1 = createSprite(random(0,400),random(0,400),10,10);
+ponto2 = createSprite(random(0,400),random(0,400),10,10);
+ponto1.shapeColor = 'red';
+ponto2.shapeColor = 'red';
+ponto1.visible = false;
+ponto2.visible = false;
 }
-
-
 
 
 function draw(){
@@ -49,23 +73,24 @@ if(estado === "Fase2"){
     background(fundo2);
 }
 
-
-if(keyDown("UP")){
-    jogador.y -=2;
+if(estado === "Fase3"){
+    background(fundo3);
 }
 
-if(keyDown("Down")){
-    jogador.y +=2;
+if(estado === "win"){
+    background("blue");
+    textAlign(CENTER,CENTER);
+    textSize(30);
+    fill("white");
+    text('you win',200,200);
+    
 }
 
-if(keyDown("LEFT")){
-    jogador.x -=2;
-}
+moveJogador();
 
-if(keyDown("RIGHT")){
-    jogador.x +=2;
-}
-
+jogador.bounceOff(borda);
+Pc2.bounceOff(borda);
+Pc3.bounceOff(borda);
 
 if(jogador.isTouching(Grupoparede1)){
     jogador.x = 50;
@@ -78,8 +103,8 @@ if(jogador.isTouching(parede11)||jogador.isTouching(parede12)){
     Grupoparede1.destroyEach();
     parede11.destroy();
     parede12.destroy();
-    Pc.x = 350;
-    Pc.y = 300;
+    Pc1.destroy();
+    Pc2.visible = true;
     estado = "Fase2";
     Fase2();
 }
@@ -87,18 +112,70 @@ if(jogador.isTouching(parede11)||jogador.isTouching(parede12)){
 if(estado === "Fase2"){
     jogador.changeAnimation("Eren",Titan_Fundador);
     jogador.scale = 0.2;
-    Pc.changeAnimation("Blindado",Titan_Blindado);
-    Pc.scale = 0.08;
-    Pc.velocityX = Math.round(random(-8,8));
-    Pc.velocityY = Math.round(random(-8,8));
-    Pc.bounceOff(Grupoparede2);
+    Pc2.bounceOff(Grupoparede2);
+    ponto1.visible = true;
+    ponto2.visible = true;
+    if(jogador.isTouching(ponto1)){
+        ponto1.destroy();
+        placar++;
+    }
+    if(jogador.isTouching(ponto2)){
+        ponto2.destroy();
+        placar++;
+    }
+    if(jogador.isTouching(Pc2)){
+        jogador.x = 50;
+        jogador.y = 50;
+    }
+    if(placar === 2){
+        estado = "Fase3";
+        Fase3();
+        Grupoparede2.destroyEach();
+        Pc2.destroy();
+        jogador.x = 350;
+        jogador.y = 350;
+    }
+}
+if(estado === 'Fase3'){
+    jogador.changeAnimation("asta", Asta);
+    jogador.scale = 0.08;
+    Pc3.visible = true;
+    if(jogador.isTouching(Grupoparede3) || jogador.isTouching(Pc3)){
+        jogador.x = 350;
+        jogador.y = 350; 
+    }
+    if(jogador.isTouching(parede19)){
+        estado = 'win';
+        Pc3.destroy();
+        jogador.destroy();
+        Grupoparede3.destroyEach();
+        parede19.destroy();
+    }
 }
 
 drawSprites();
 }
 
+function moveJogador(){
+    if(keyDown("UP")){
+        jogador.y -=2;
+    }
+    
+    if(keyDown("Down")){
+        jogador.y +=2;
+    }
+    
+    if(keyDown("LEFT")){
+        jogador.x -=2;
+    }
+    
+    if(keyDown("RIGHT")){
+        jogador.x +=2;
+    }
+}
+
 function Fase1(){
-    Grupoparede1 = createGroup();
+Grupoparede1 = createGroup();
 parede1 = createSprite(50,87,100,25);
 parede2 = createSprite(112,200,25,200);
 parede3 = createSprite(12,250,25,325);
@@ -124,7 +201,7 @@ Grupoparede1.add(parede10);
 }
 
 function Fase2(){
-    Grupoparede2 = createGroup();
+Grupoparede2 = createGroup();
 parede13 = createSprite(200,0,400,10);
 parede14 = createSprite(400,200,10,400);
 parede15 = createSprite(200,400,400,10);
@@ -134,3 +211,13 @@ Grupoparede2.add(parede14);
 Grupoparede2.add(parede15);
 Grupoparede2.add(parede16);
 }
+function Fase3(){
+    Grupoparede3 = createGroup();
+    parede17 = createSprite(150,130,300,15);
+    parede18 = createSprite(250,280,300,15);
+    parede19 = createSprite(50,50,50,50);
+    parede19.addImage(trofeu);
+    parede19.scale = 0.08;
+    Grupoparede3.add(parede17);
+    Grupoparede3.add(parede18);
+    }
